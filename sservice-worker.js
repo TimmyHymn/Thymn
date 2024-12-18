6848d3ad180e4c64680a38b35ef69c3395913e9b
TimmyHymn <ckgeniekimo@yahoo.com.tw>
2024-12-18 22:08:49 +0800

    Add files via upload

---
+++ sservice-worker.js
@@ -0,0 +1,23 @@
+﻿// sservice-worker.js
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('v1').then(cache => {
+            return cache.addAll([
+                '/Thymn/H1S.html',
+                '/Thymn/smanifest.json',
+                '/Thymn/style.css',
+                '/Thymn/script.js',
+                '/Thymn/icon-192x192.png',
+                '/Thymn/icon-512x512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
