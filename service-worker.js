535a4c44b650a1e0a1ac701dd5b9596d7763f0ab
TimmyHymn <ckgeniekimo@yahoo.com.tw>
2024-12-18 15:21:37 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿// service-worker.js
+// service-worker.js
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('v1').then(cache => {
@@ -9,15 +9,23 @@ self.addEventListener('install', event => {
                 '/Thymn/script.js',
                 '/Thymn/icon-192x192.png',
                 '/Thymn/icon-512x512.png'
-            ]);
+            ]).catch(error => {
+                console.error('Caching failed:', error);
+            });
         })
     );
+    console.log('Service Worker installed');
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).catch(error => {
+                console.error('Fetching failed:', error);
+            });
+        }).catch(error => {
+            console.error('Matching cache failed:', error);
         })
     );
+    console.log('Fetching:', event.request.url);
 });
