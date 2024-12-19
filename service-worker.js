ca65a6520341d3e4876c3773854c3dbe649cd3a9
TimmyHymn <ckgeniekimo@yahoo.com.tw>
2024-12-19 15:39:17 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,7 @@
-const CACHE_NAME = 'v2'; // 使用新版本號來強制更新緩存
+const CACHE_NAME = 'v0'; // 使用新版本號來強制更新緩存
 const CACHE_FILES = [
     '/Thymn/H1.html',
     '/Thymn/manifest.json',
-    '/Thymn/style.css',
-    '/Thymn/script.js',
     '/Thymn/icon-192x192.png',
     '/Thymn/icon-512x512.png'
 ];
