edccac79cf31088d38a3e2c8f9268a96bca9ee99
TimmyHymn <ckgeniekimo@yahoo.com.tw>
2024-12-19 15:57:19 +0800

    Update tservice-worker.js

--- tservice-worker.js
+++ tservice-worker.js
@@ -1,23 +1,48 @@
-﻿// service-worker.js
+const CACHE_NAME = 'v0'; // 使用新版本號來強制更新緩存
+const CACHE_FILES = [
+    '/Thymn/H12.html',
+    '/Thymn/manifest.json',
+    '/Thymn/icon-192x192.png',
+    '/Thymn/icon-512x512.png'
+];
+
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('v1').then(cache => {
-            return cache.addAll([
-                '/Thymn/H12.html',
-                '/Thymn/tmanifest.json',
-                '/Thymn/style.css',
-                '/Thymn/script.js',
-                '/Thymn/icon-192x192.png',
-                '/Thymn/icon-512x512.png'
-            ]);
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(CACHE_FILES);
         })
     );
+    console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    console.log('Service Worker activated');
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).then(fetchResponse => {
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        }).catch(() => {
+            return caches.match('/Thymn/H12.html');
         })
     );
+    console.log('Fetching:', event.request.url);
 });
